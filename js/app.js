bd07ca7825ffe2d3d3aabdf7c77025177c560853
yeye07 <Yeye@Edwins-MacBook-Pro.local>
2017-04-28 16:47:23 -0500

    Everything has evoled

--- js/app.js
+++ js/app.js
@@ -1,10 +1,59 @@
+
+
 $(document).ready(function() {
- $('div.ryu').mouseenter(function(){
-		$('div.ryu-still').hide();
-		$('div.ryu-ready').show();
+	  $('.ryu').mouseenter(function(){
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
 	})
-.mouseleave(function(){
-	$('div.ryu-still').hide();
-		$('div.ryu-ready').show();
+     .mouseleave(function(){
+     	$('.ryu-still').show();
+		$('.ryu-ready').hide();
+})
+		 .mousedown(function(){
+         playHadouken();
+         $('.ryu-ready').hide();
+         $('.ryu-throwing').show();
+         $('.hadouken').finish().show().animate(
+           {'left': '1200px'},
+           500,
+           function(){
+           	$(this).hide();
+           	$(this).css('left','520px');
+           });
+})
+
+
+		 .mouseup(function(){
+     	$('.ryu-throwing').hide();
+     	$('.ryu-ready').show();
+
+ });
 });
-});
+
+
+
+
+function playHadouken(){
+	$('#hadouken-sound') [0].volume = 0.5;
+	$('#hadouken-sound') [0].load();
+	$('#hadouken-sound') [0].play();
+
+}
+
+
+
+$(document).keydown(function(e) {
+    if (e.keyCode == 88) {
+    	   $('.ryu-cool').show();
+    	   $('.ryu-ready').hide();
+    	   $('.ryu-still').hide();
+  
+    }
+    }).keyup(function(e) {
+      if (e.keyCode == 88) {
+     $('.ryu-cool').hide();
+      $('.ryu-ready').show();
+
+    }
+  });
+
